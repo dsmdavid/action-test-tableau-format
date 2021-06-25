620a6e28593b526138845cc62be23be38ce001ec
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2021-06-25 23:07:10 +0100

    feat: new

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,38 +5,12 @@ echo ::set-ouput name=time::$time
 echo "The modified files are:"
 cat modified_files.txt
 
-echo "logging ENV"
-env
-echo "##########"
-echo 'pwd'
-pwd
-echo 'ls'
-ls -al
-echo '£££££'
-cd /
-ls -al
-echo "finding file test_all.py"
-find . -name test_all.py
-echo "end finding"
-
-echo " Script to run:"
-cat ./app/test_all.py
-
-echo "Calling script: \n"
+echo "Calling script: python ./app/test_all.py \n"
 # /bin/bash
 python ./app/test_all.py
 echo "####### "
-echo "#######"
-echo "#######"
-echo "#######"
-echo "finding commands.sh"
-find . -name commands.sh
-
-echo "commands to be ran:"
+echo "These commands will be run:"
 cat ./commands.sh
-echo "#######"
-echo "#######"
-echo "#######"
 echo "#######"
 chmod +x ./commands.sh
 ./commands.sh
