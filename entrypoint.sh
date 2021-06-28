6b97ad96366b181558a92b0422a4b5aee49fc251
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2021-06-28 10:47:35 +0100

    fix: add debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,24 @@ echo ::set-ouput name=time::$time
 echo "The modified files are:"
 cat modified_files.txt
 cd /
+echo "::group::Debug info"
+echo "Set ACTIONS_STEP_DEBUG variable to True to see these logs"
 echo "::debug::Listing files"
 echo "::debug::$(ls -al)"
 echo "::debug::finding test_all.py"
 echo "::debug::$(find . -name test_all.py)"
-echo "Calling script: python ./app/src/test_all.py \n"
+echo "::endgroup::"
 
+echo "Calling script: python ./app/src/test_all.py \n"
 python ./app/src/test_all.py
 echo "####### "
 echo "::debug::These commands will be run:"
 echo "::debug::$(cat ./commands.sh)"
 echo "#######"
+echo "::group::Format Validation"
 chmod +x ./commands.sh
 ./commands.sh
+echo "::endgroup::"
 echo "::debug::The output is:"
 echo "::debug::$(find . -name output.txt)"
 echo "::debug::$(cat /github/workspace/outputs.txt)"
